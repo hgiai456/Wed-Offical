b153539e80f5517278e660368987f980111b8f3a
23thyu <hieungao2004@gmail.com>
2024-08-28 18:26:12 +0700

    First-commit
    Have more fail

---
+++ QLCoffee/QLCoffee/Controllers/StaffController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLCoffee.Models;
+
+namespace QLCoffee.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        QuanLyQuanCoffeeEntities database = new QuanLyQuanCoffeeEntities();
+        public ActionResult Index()
+        {
+            return View(database.NHANVIENs.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(NHANVIEN nhanvien)
+        {
+            database.NHANVIENs.Add(nhanvien);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            return View(database.NHANVIENs.Where(s => s.MaNV == id).FirstOrDefault());
+        }
+        public ActionResult Edit(string id)
+        {
+            return View(database.NHANVIENs.Where(s => s.MaNV == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(string id,NHANVIEN nhanvien)
+        {
+            database.Entry(nhanvien).State=System.Data.Entity.EntityState.Modified;
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            return View(database.NHANVIENs.Where(s => s.MaNV == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(string id,NHANVIEN nhanvien)
+        {
+            nhanvien = database.NHANVIENs.Where(s => s.MaNV == id).FirstOrDefault();
+            database.NHANVIENs.Remove(nhanvien);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QLCoffee/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿insert into	NHANVIEN
+values('NV01',N'Nguyễn Trọng Hiếu','0949439756','Nam',N'17 Giải Phóng','2004-10-23',N'Quản Lý')
